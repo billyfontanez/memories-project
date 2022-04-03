d67ade8a07bd485f1566cff32019abfc80956849
William Fontanez <william.r.fontanez@gmail.com>
2022-04-03 15:54:54 -0500

    Update index.js
    Linked server to MongoDB Atlas

--- server/index.js
+++ server/index.js
@@ -2,3 +2,14 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://williamf:PA$$word11!!@cluster0.3yp5k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL).then(()=>{console.log(`Server is running on port ${PORT}`)})
